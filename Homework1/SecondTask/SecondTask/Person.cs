a526fbd6d1f42b632c107b7722e0e6bd8a3e5b5c
naismore <124817694+naismore@users.noreply.github.com>
2023-07-09 21:59:17 +0300

    Homework 1

---
+++ Homework1/SecondTask/SecondTask/Person.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace SecondTask
+{
+    class Person
+    {
+
+        private string _name = "undefined";
+        private int _age = 0;
+        private string _email = "undefined";
+        private string _githublink = "undefined";
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine($"ОШИБКА: Входеная строка не должна состоять только из символов разделителей");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0 || value < 130)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА: Возраст должен быть больше 0, но меньше 130");
+                }
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value) && value.Contains("@"))
+                {
+                    _email = value;
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА: Входеная строка должна содержать \"@\" и не состоять только из символов разделителей");
+                }
+            }
+        }
+
+        public string Githublink
+        {
+            get
+            {
+                return _githublink;
+            }
+
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    _githublink = value;
+                }
+                else
+                {
+                    Console.WriteLine("ОШИБКА: Строка не должна быть пустой");
+                }
+            }
+        }
+
+        public void WriteInfo()
+        {
+            Console.WriteLine($"ФИО: {_name}\n  Возраст: {_age}\n  Email: {_email}\n  Github: {_githublink}");
+        }
+
+    }
+}
---
+++ Homework1/SecondTask/SecondTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.Write("Введите Ваше имя: ");
+            person.Name = Console.ReadLine();
+            Console.Write("Введите Ваш возраст: ");
+            try
+            {
+                person.Age = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Write("Введите Ваш email: ");
+            person.Email = Console.ReadLine();
+            Console.Write("Введите ссылку на Ваш GitHub: ");
+            person.Githublink = Console.ReadLine();
+            Console.WriteLine();
+            person.WriteInfo();
+            Console.ReadKey();
+        }
+    }
+}
